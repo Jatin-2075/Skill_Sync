f8c8d8f3fc24be543d9bb901d1cd65ad4016f327
jatin <jatin2372005@gmail.com>
2025-12-27 05:10:59 +0530

    idk let's see

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { LayoutDashboard, Book, BookHeart } from "lucide-react";
+
+import "../Style/navbar.css"
+
 const Navbar = () => {
+    const [width, setWidth] = useState<number>(0);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const Nav = [
+        { path: "/dashboard", name: "Dashboard", icon: LayoutDashboard },
+        { path: "/community", name: "Community", icon: Book },
+        { path: "/knowns", name: "Knows", icon: BookHeart },
+    ];
+
     return (
-        <div>
-            navbar
-        </div>
-    )
-}
+        <nav className="navbar">
+            <div className="navbar-container">
+                {Nav.map(({ path, name, icon: Icon }) => (
+                    <NavLink key={path} to={path} className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+                        <Icon size={18} className="nav-icon" />
+                        {width > 786 && <span className="nav-text">{name}</span>}
+                    </NavLink>
+                ))}
+            </div>
+        </nav>
+
+    );
+};
 
 export default Navbar;
